9442bc186142023b197c6bac4f0c5a7cb049c282
Edu49 <eduardgabriel.donisan@iismajoranamoncalieri.edu.it>
2024-11-06 13:45:22 +0100

    Create codice.py

---
+++ codice.py
@@ -0,0 +1,9 @@
+a = int(input("Inserisci il numeratore: "))
+b = int(input("Inserisci il denominatore: "))
+
+if b != 0:
+    divisione = a / b
+    print("La divisione è: ")
+    print(divisione)
+else:
+    print("Errore: il denominatore non può essere zero.")
